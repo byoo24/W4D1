c52596caa8b73ad1166f0b2715b93e40c479bec9
byoo24 <byoo.dev@gmail.com>
2019-08-25 14:42:46 -0400

    Sudoku Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sudoku
+
+Inline Sudoku Project
--- project_demo/db/migrate/20190429190516_add_index.rb
+++
@@ -1,5 +0,0 @@
-class AddIndex < ActiveRecord::Migration[5.2]
-  def change
-    add_index(:artworks, [:artist_id, :title], unique: true)
-  end
-end
